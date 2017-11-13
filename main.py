49c3c7f04b2f37bb4ee0fab7588c65285caca6fd
Mike Jato <mike@macbook-de-mike-jato.local>
2017-11-13 16:48:05 -0600

    iteraciones para reinversion

--- main.py
+++ main.py
@@ -2,61 +2,109 @@
 import pandas as pd
 from datetime import timedelta, datetime
 
+def obtener_bid_ask(df_data, end_date, periodo):
+    
+    # determino periodo de muestra
+    beg_date = end_date + timedelta(days=-period)
+    print('periodo muestra: {} - {}'.format(beg_date, end_date))
+ 
+    # obtengo muestra de datos a utilizar
+    df_sample = df_data[(df_data.Date >= beg_date) & (df_data.Date <= end_date)]
+
+    # calcular desviacion estandar
+    close_std  = float(df_sample.Close.std())
+    print('desviacion std: {}'.format(close_std))
+
+    # establecer precios de compra y venta
+    end_date = df_sample.Date.max()
+    close_price = float(df_sample[(df_sample.Date == end_date)].Close)
+    goal_bid_price = close_price - close_std
+    goal_ask_price = close_price + close_std
+
+    return goal_bid_price, goal_ask_price
+
+def identificar_compra(df_data, end_date, goal_bid_price):
+    
+    df_bid = df_data[(df_data.Date > end_date) & (df_data.Close <= goal_bid_price)]
+
+    if df_bid.shape[0] > 0:
+        bid_date = df_bid.iloc[0].Date
+        bid_price = float(df_bid.iloc[0].Close)
+        print('compra!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+        print('fecha de compra: {}'.format(bid_date))
+        print('precio de compra: {}'.format(bid_price))
+
+        return bid_date, bid_price
+
+    else:
+        raise Exception('imposible comprar')
+
+def identificar_venta(df_data, bid_date, goal_ask_price):
+    
+    df_ask = df_data[(df_data.Date > bid_date) & (df_data.Close >= goal_ask_price)]
+    if df_ask.shape[0] > 0:
+        ask_date = df_ask.iloc[0].Date
+        ask_price = float(df_ask.iloc[0].Close)
+        print('venta!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+        print('fecha de venta: {}'.format(ask_date))
+        print('precio de venta: {}'.format(ask_price))
+
+        return ask_date, ask_price
+        
+    else:
+        raise Exception('imposible vender')
+
 def main(capital, period):
+    
+    beg_capital = capital           # capital inicial
 
     # leer historico de precios
     df_data = pd.read_csv('/Users/mike/calculadora-alsea2/data/ALSEA.MX.csv')   
     df_data.Date = df_data.Date.apply(lambda x: datetime.strptime(x, '%Y-%m-%d'))
 
-    # determino perio de muestra
-    beg_date = df_data.Date.min()
-    end_date = beg_date + timedelta(days=period)
-    print('periodo muestra: {} - {}'.format(beg_date, end_date))
+    # determino periodo de muestra
+    date = df_data.Date.min() + timedelta(days=period)
+    fTrade = True
+    stocks = 0
 
-    # obtengo muestra de datos a utilizar
-    df_sample = df_data[(df_data.Date >= beg_date) & (df_data.Date <= end_date)]
-
-    # calcular desviacion estandar
-    close_std  = df_sample.Close.std()
-    print('desviacion std: {}'.format(close_std))
-
-    # establecer precios de compra y venta
-    end_date = df_sample.Date.max()
-    close_price = df_sample[(df_sample.Date == end_date)].Close
-    goal_bid_price = float(close_price) - float(close_std)
-    goal_ask_price = float(close_price) + float(close_std)
-
-    print('meta de compra: {}'.format(goal_bid_price))
-    print('meta de venta: {}'.format(goal_ask_price))
-
-    # establecer momento de compra
-    df_bid = df_data[(df_data.Date > end_date) & (df_data.Close <= goal_bid_price)]
-    if df_bid.shape[0] > 0:
-        bid_date = df_bid.iloc[0].Date
-        bid_price = float(df_bid.iloc[0].Close)
-        stocks = capital / bid_price
-        print('fecha de compra: {}'.format(bid_date))
-        print('precio de compra: {}'.format(bid_price))
-        print('acciones: {}'.format(stocks))
-
-    # establecer momento de venta
-    df_ask = df_data[(df_data.Date > bid_date) & (df_data.Close >= goal_ask_price)]
-    if df_ask.shape[0] > 0:
-        ask_date = df_ask.iloc[0].Date
-        ask_price = float(df_ask.iloc[0].Close)
-        capital = stocks * ask_price
-        print('fecha de venta: {}'.format(ask_date))
-        print('precio de venta: {}'.format(ask_price))
-        print('capital: {}'.format(capital))
-        print('rend directo: {}%'.format((ask_price / bid_price - 1) * 100))
-        # print('dias inversion: {}'.format())
+    while fTrade:
         
+        try:
 
-    # TODO: determinar precio de venta
-    # TODO: vender
+            # obtengo precios de compra y venta
+            goal_bid_price, goal_ask_price = obtener_bid_ask(df_data, date, period)
+            print('meta de compra: {}'.format(goal_bid_price))
+            print('meta de venta: {}'.format(goal_ask_price))
+
+            # establecer momento de compra
+            bid_date, bid_price = identificar_compra(df_data, date, goal_bid_price)
+            stocks = int(capital / bid_price)
+            print('acciones: {} ({} / {})'.format(stocks, capital, bid_price))
+            capital -= stocks * bid_price
+
+            # establecer momento de venta
+            ask_date, ask_price = identificar_venta(df_data, bid_date, goal_ask_price)
+            capital += stocks * ask_price
+            print('capital: {}'.format(capital))
+            print('rend: {}%'.format((ask_price / bid_price - 1) * 100))
+            stocks = 0
+
+            # siguiene ciclo
+            date = ask_date
+
+        except Exception as exc:
+            print(str(exc))
+            fTrade = False
+
+    last_price = float(df_data[(df_data.Date == df_data.Date.max())].Close)
+    last_capital = stocks * last_price + capital
+
+    print('capital final: ${} (${} x {} + ${})'.format(last_capital, last_price, stocks, capital))
+    print('rend: {}%'.format((last_capital / beg_capital - 1) * 100))
+
     return True
 
 if __name__ == '__main__':
     capital = float(raw_input('capital: '))
-    periodo = int(raw_input('periodo: '))
-    print(main(capital, periodo))
+    period = int(raw_input('periodo: '))
+    print(main(capital, period))
